7cb6d66bde0691d5f575583306e54f12698d35c9
Frank Wang <frankwdy11@gmail.com>
2023-04-23 22:59:15 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# game-loft-front
+# game-loft frontend
+
+## Environment
+
+`Node >= 6`
+
+## Start
+
+ - Clone or download this repository
+ - Enter your local directory, and install dependencies:
+
+``` bash
+yarn
+```
+
+## Develop
+
+``` bash
+# serve with hot reload at localhost:8010
+npm run dev
+```
+
+## Build
+
+``` bash
+# build for production with minification
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="register">注册</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    register () {
+      this.$notify({
+        title: '注册动作',
+        type: 'success',
+        message: '注册触发!',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
